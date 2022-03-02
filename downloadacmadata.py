3a5e7d9cad4692dd0d617ced7c840a179584d012
Adon Metcalfe <adonm@fastmail.fm>
2022-03-02 16:31:59 +0800

    cleaner coverage

--- downloadacmadata.py
+++ downloadacmadata.py
@@ -4,6 +4,9 @@ from subprocess import run
 if not os.path.exists("spectra_rrl.zip"):
     run("curl -k https://web.acma.gov.au/rrl-updates/spectra_rrl.zip -o spectra_rrl.zip", shell=True)
 if not os.path.exists("spectra_rrl.gpkg"):
-    run("ogr2ogr -overwrite spectra_rrl.gpkg spectra_rrl.vrt client licence device_details site", shell=True)
+    run("ogr2ogr spectra_rrl.gpkg spectra_rrl.vrt client licence device_details site", shell=True)
 if not os.path.exists("spectra_rrl_query.gpkg"):
-    run("ogr2ogr -overwrite -unsetFid spectra_rrl_query.gpkg spectra_rrl_query.vrt", shell=True)
+    # The order here is important as layers depend on previous calcs
+    run("ogr2ogr -unsetFid spectra_rrl_query.gpkg spectra_rrl_query.vrt aggregated_sites", shell=True)
+    run("ogr2ogr -update -unsetFid spectra_rrl_query.gpkg spectra_rrl_query.vrt network_sites", shell=True)
+    run("ogr2ogr -update -unsetFid spectra_rrl_query.gpkg spectra_rrl_query.vrt network_coverage", shell=True)
